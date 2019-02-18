21178ac58e3cdcdbb2a7dd51069955c2673dd43b
Mike Fix <mrfix84@gmail.com>
2019-02-18 11:00:51 -0800

    more test cases

--- test/index.test.js
+++ test/index.test.js
@@ -5,56 +5,76 @@ import moment from 'moment'
 import Calendar from '..'
 
 function render(options) {
-  return rtlRender(<Calendar {...options} />)
+  return rtlRender(React.createElement(Calendar, options, options.children))
 }
 
-test('basic rendering', () => {
+const BASIC_CASES = [
+  [
+    35,
+    [
+      '2019-01-30',
+      '2019-01-31',
+      '2019-02-01',
+      '2019-02-02',
+      '2019-02-03',
+      '2019-02-04',
+      '2019-02-05',
+      '2019-02-06',
+      '2019-02-07',
+      '2019-02-08',
+      '2019-02-09',
+      '2019-02-10',
+      '2019-02-11',
+      '2019-02-12',
+      '2019-02-13',
+      '2019-02-14',
+      '2019-02-15',
+      '2019-02-16',
+      '2019-02-17',
+      '2019-02-18',
+      '2019-02-19',
+      '2019-02-20',
+      '2019-02-21',
+      '2019-02-22',
+      '2019-02-23',
+      '2019-02-24',
+      '2019-02-25',
+      '2019-02-26',
+      '2019-02-27',
+      '2019-02-28',
+      '2019-03-01',
+      '2019-03-02',
+      '2019-03-03',
+      '2019-03-04',
+      '2019-03-05'
+    ]
+  ],
+  [4, ['2019-02-18', '2019-02-19', '2019-02-20', '2019-02-21']],
+  [
+    7,
+    [
+      '2019-02-17',
+      '2019-02-18',
+      '2019-02-19',
+      '2019-02-20',
+      '2019-02-21',
+      '2019-02-22',
+      '2019-02-23'
+    ]
+  ]
+]
+
+test.each(BASIC_CASES)('basic rendering with  %i days in view', (numDays, daysInView) => {
   const children = jest.fn(() => null)
-  const referenceDate = moment('02-17-2019', 'MM-DD-YYYY')
-  render({ referenceDate, children })
+  const referenceDate = moment('02-18-2019', 'MM-DD-YYYY')
+  render({ initialReferenceDate: referenceDate, numDays, children })
 
   const firstCall = children.mock.calls[0][0]
 
   const flattenedDays = [].concat(...firstCall.days)
 
-  expect(flattenedDays.length).toBe(35)
-  expect(flattenedDays.map(d => d.date).map(iso => iso.split('T')[0])).toEqual([
-    '2019-01-31',
-    '2019-02-01',
-    '2019-02-02',
-    '2019-02-03',
-    '2019-02-04',
-    '2019-02-05',
-    '2019-02-06',
-    '2019-02-07',
-    '2019-02-08',
-    '2019-02-09',
-    '2019-02-10',
-    '2019-02-11',
-    '2019-02-12',
-    '2019-02-13',
-    '2019-02-14',
-    '2019-02-15',
-    '2019-02-16',
-    '2019-02-17',
-    '2019-02-18',
-    '2019-02-19',
-    '2019-02-20',
-    '2019-02-21',
-    '2019-02-22',
-    '2019-02-23',
-    '2019-02-24',
-    '2019-02-25',
-    '2019-02-26',
-    '2019-02-27',
-    '2019-02-28',
-    '2019-03-01',
-    '2019-03-02',
-    '2019-03-03',
-    '2019-03-04',
-    '2019-03-05',
-    '2019-03-06'
-  ])
+  expect(flattenedDays.length).toBe(numDays)
+  expect(flattenedDays.map(d => d.date).map(iso => iso.split('T')[0])).toEqual(daysInView)
   expect(children).toHaveBeenCalledWith(
     expect.objectContaining({
       // referenceDate,
