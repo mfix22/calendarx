318b96d77f562210c255844963c2edb10f1dc846
Mike Fix <mrfix84@gmail.com>
2020-11-14 12:25:04 -0800

    finish d.ts

--- calendarx.d.ts
+++ calendarx.d.ts
@@ -1,20 +1,72 @@
-export = Calendarx;
+import * as React from "react";
 
-declare function Calendarx(e: any): any;
+export type DateLike = Date | string | number
+
+export interface Event {
+    date: Date;
+}
+
+export type View = 'year' | 'month' | 'week' | 'day'
+
+export type ButtonPropsGetter = () => { label: string, onClick: () => void }
+
+export type Day = {
+    date: Date,
+    events: Event[],
+    isToday: boolean,
+    isSame: (view: View) => boolean
+}
+
+export type Properties = {
+    date: Date,
+    days: Day[][],
+    headers: string[],
+    view: View,
+    numDays: number,
+    jump: (n: number, v: View) => void,
+    goToNext: () => void,
+    goToPrev: () => void,
+    goToToday: () => void,
+    goToDate: (date: DateLike) => void,
+    getPrevButtonProps: ButtonPropsGetter,
+    getNextButtonProps: ButtonPropsGetter,
+    getTodayButtonProps: ButtonPropsGetter,
+    setNumDays: (numDays: number) => void
+}
+
+export interface Options {
+    initialDate?: DateLike;
+    initialNumDays?: number;
+    date?: DateLike;
+    numDays?: number,
+    events?: Event[];
+    weekStartsOn?: number;
+    headers?: string[];
+}
+
+export type Props = Options & {
+    children?: (properties: Properties) => React.ReactNode
+}
+
+export default Calendarx;
+
+declare function Calendarx(props: Props): React.ReactNode;
 
 declare namespace Calendarx {
+    function useCalendar(options: Options): Properties;
+
     const days: {
-        FRIDAY: number;
-        MONDAY: number;
-        SATURDAY: number;
         SUNDAY: number;
-        THURSDAY: number;
+        MONDAY: number;
         TUESDAY: number;
         WEDNESDAY: number;
+        THURSDAY: number;
+        FRIDAY: number;
+        SATURDAY: number;
     };
 
     const defaultProps: {
-        events: any[];
+        events: Event[];
         headers: string[];
         initialNumDays: number;
         weekStartsOn: number;
@@ -26,8 +78,5 @@ declare namespace Calendarx {
         MONTH: string;
         YEAR: string;
     };
-
-    function useCalendar(e: any): any;
-
 }
 
