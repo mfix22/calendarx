6971a7f319a6b91da0614f1e48b981531456a0e2
Mike Fix <mrfix84@gmail.com>
2020-11-17 20:32:30 -0800

    formatting

--- calendarx.d.ts
+++ calendarx.d.ts
@@ -1,51 +1,64 @@
-import * as React from "react";
+import * as React from 'react';
 
-export type DateLike = Date | string | number
+export type DateLike = Date | string | number;
 
-export type Event = { date: DateLike } | { startDate: DateLike, endDate: DateLike }
+export type Event =
+  | { date: DateLike }
+  | { startDate: DateLike; endDate: DateLike };
 
-export type View = 'year' | 'month' | 'week' | 'day'
+export type View = 'year' | 'month' | 'week' | 'day';
 
-export type ButtonPropsGetter = () => { label: string, onClick: () => void }
+export type ButtonPropsGetter = () => { label: string; onClick: () => void };
 
 export type Day = {
-    date: Date,
-    events: Event[],
-    isToday: boolean,
-    isSame: (view: View) => boolean
-}
+  date: Date;
+  events: Event[];
+  isToday: boolean;
+  isSame: (view: View) => boolean;
+};
 
-export type Weekday = 0 | 1 | 2 | 3 | 4 | 5 | 6
+export type Weekday = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export type JumpUnit =
+  | View
+  | 'years'
+  | 'y'
+  | 'months'
+  | 'M'
+  | 'weeks'
+  | 'w'
+  | 'days'
+  | 'd';
 
 export type Properties = {
-    date: Date,
-    days: Day[][],
-    headers: string[],
-    view: View,
-    numDays: number,
-    jump: (n: number, v: View) => void,
-    goToNext: () => void,
-    goToPrev: () => void,
-    goToToday: () => void,
-    goToDate: (date: DateLike) => void,
-    getPrevButtonProps: ButtonPropsGetter,
-    getNextButtonProps: ButtonPropsGetter,
-    getTodayButtonProps: ButtonPropsGetter,
-    setNumDays: (numDays: number) => void
-}
+  date: Date;
+  days: Day[][];
+  headers: string[];
+  view: View;
+  numDays: number;
+  jump: (n: number, v?: JumpUnit) => void;
+  goToNext: () => void;
+  goToPrev: () => void;
+  goToToday: () => void;
+  goToDate: (date: DateLike) => void;
+  getPrevButtonProps: ButtonPropsGetter;
+  getNextButtonProps: ButtonPropsGetter;
+  getTodayButtonProps: ButtonPropsGetter;
+  setNumDays: (numDays: number) => void;
+};
 
 export interface Options {
-    initialDate?: DateLike;
-    initialNumDays?: number;
-    date?: DateLike;
-    numDays?: number,
-    events?: Event[];
-    weekStartsOn?: Weekday;
-    headers?: string[];
+  initialDate?: DateLike;
+  initialNumDays?: number;
+  date?: DateLike;
+  numDays?: number;
+  events?: Event[];
+  weekStartsOn?: Weekday;
+  headers?: string[];
 }
 
 export interface Props extends Options {
-    children: (properties: Properties) => React.ReactNode
+  children: (properties: Properties) => React.ReactNode;
 }
 
 export default Calendarx;
@@ -53,30 +66,29 @@ export default Calendarx;
 declare function Calendarx(props: Props): React.ReactNode;
 
 declare namespace Calendarx {
-    function useCalendar(options?: Options): Properties;
+  function useCalendar(options?: Options): Properties;
 
-    const days: {
-        SUNDAY: 0;
-        MONDAY: 1;
-        TUESDAY: 2;
-        WEDNESDAY: 3;
-        THURSDAY: 4;
-        FRIDAY: 5;
-        SATURDAY: 6;
-    };
+  const days: {
+    SUNDAY: 0;
+    MONDAY: 1;
+    TUESDAY: 2;
+    WEDNESDAY: 3;
+    THURSDAY: 4;
+    FRIDAY: 5;
+    SATURDAY: 6;
+  };
 
-    const defaultProps: {
-        events: Event[];
-        headers: string[];
-        initialNumDays: number;
-        weekStartsOn: Weekday;
-    };
+  const defaultProps: {
+    events: Event[];
+    headers: string[];
+    initialNumDays: number;
+    weekStartsOn: Weekday;
+  };
 
-    const views: {
-        DAY: 'day';
-        WEEK: 'week';
-        MONTH: 'month';
-        YEAR: 'year';
-    };
+  const views: {
+    DAY: 'day';
+    WEEK: 'week';
+    MONTH: 'month';
+    YEAR: 'year';
+  };
 }
-
